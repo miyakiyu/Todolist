85464b7027a15056c58845c11dd8c6876b5c479d
miyakiyu <sherry910509@gmail.com>
2025-04-03 10:02:31 +0800

    addTodo and removeTodo function

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue';
+//bind the input newTodo
+const newTodo = ref(''); 
+//array of todos
+const todos = ref([]);
+//addTodo function
+const addTodo = () =>{
+  if(newTodo.value.trim() !== ''){
+    todos.value.push({ text: newTodo.value});
+    newTodo.value = '';
+  }
+};
+//removeTodo function
+const removeTodo = (index) => {
+  //from index delete 1 element
+  todos.value.splice(index, 1);
+};
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Todolist</h1>
+      <div>
+      <input v-model = "newTodo" placeholder="Add new Todo"/>
+      <button v-on:click="addTodo">Add</button>
+    </div>
+
+    <!--todo-list-->
+    <ul>
+      <li v-for = "todo in todos" :key = "index">
+        {{ todo.text }}
+        <button v-on:click="removeTodo(index)">X</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+.container {
+  max-width: 400px;
+  margin: auto;
+  text-align: center;
+}
+</style>
